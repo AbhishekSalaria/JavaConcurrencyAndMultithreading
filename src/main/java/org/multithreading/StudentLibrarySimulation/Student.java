c8311db02b2723c347248c7bad428fd3430860c0
Abhishek Salaria <abhisheksalaria26@gmail.com>
2023-05-31 14:28:22 +0530

    Adding Student Library Simulation Using Reentrant Locks

---
+++ src/main/java/org/multithreading/StudentLibrarySimulation/Student.java
@@ -0,0 +1,34 @@
+package org.multithreading.StudentLibrarySimulation;
+
+import java.util.Random;
+
+public class Student implements Runnable{
+
+    private int id;
+    private Book[] books;
+
+    public Student(int id, Book[] books) {
+        this.id = id;
+        this.books = books;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+
+        while (true) {
+            int bookId = random.nextInt(Constants.NUMBER_OF_BOOKS);
+
+            try {
+                books[bookId].read(this);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Student #"+ id;
+    }
+}
